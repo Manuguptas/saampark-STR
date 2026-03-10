8ce83adb7fdb3bf75ce0cebd5ecda4107124a713
Manu gupta <144513877+Manuguptas@users.noreply.github.com>
2026-03-10 17:11:00 +0530

    Add files via upload

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">About Us</h1>
+
+      <p className="mt-4">
+        Saampark Technology & Research Pvt Ltd is an ISO certified
+        company providing software development, stock market
+        analysis, website development and hosting solutions.
+      </p>
+    </div>
+  );
+}
